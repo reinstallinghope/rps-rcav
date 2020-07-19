be05aaa6c3039bfdf96091d015161a0c76024897
mbhusals <mbhusals@chicagobooth.edu>
2020-07-19 21:20:23 +0000

    Got through 30 mins of video

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+  #write a ton of code
 
+  #redirect_to ("https://www.wikipedia.org")
+  #render ({:plain => "Howdy, world!"})
+  render ({ :template => "game_templates/user_rock.html.erb"})
+  end
 end
